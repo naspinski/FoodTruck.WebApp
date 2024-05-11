35b54cf26f9f2914e5468f5c588abc4d70461c48
Stan <stan.naspinski@sustainment.tech>
2024-05-10 21:35:56 -0500

    added logging options

--- Naspinski.FoodTruck.WebApp/Controllers/_BaseController.cs
+++ Naspinski.FoodTruck.WebApp/Controllers/_BaseController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.Json;
 using Elmah.Io.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Naspinski.FoodTruck.Data;
@@ -18,6 +20,17 @@ namespace Naspinski.FoodTruck.WebApp.Controllers
         protected void Log(Exception ex)
         {
             ex.Ship(HttpContext);
+        }
+
+        protected void Log(string message, params object[] objects)
+        {
+            var msg = new StringBuilder(message);
+            var opts = new JsonSerializerOptions { WriteIndented = true };
+            foreach (var obj in objects)
+            {
+                msg.AppendLine(JsonSerializer.Serialize(obj, opts));
+            }
+            Log(new Exception(msg.ToString()));
         }
     }
 }
